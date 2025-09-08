e83041db5da1a4bcca234dd771ed9bba5c749038
owenlima <owen.liima@gmail.com>
2025-09-08 18:50:02 -0300

    feat: classe edificio

--- Atividade 5 - Aula 08-09-2025/main.js
+++ Atividade 5 - Aula 08-09-2025/main.js
@@ -44,3 +44,57 @@ class Morador extends Pessoa {
         console.log(`Código de Acesso: ${this.codigoAcesso}`);
     }
 }
+
+class Edificio {
+    constructor(nome, endereco, bairro, cidade, uf) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+    get nome() {
+        return this.nome;
+    }
+
+    set nome(novoNome) {
+        this.nome = novoNome;
+    }
+
+    get endereco() {
+        return this.endereco;
+    }
+
+    set endereco(novoEndereco) {
+        this.endereco = novoEndereco;
+    }
+
+    get bairro() {
+        return this.bairro;
+    }
+
+    set bairro(novoBairro) {
+        this.bairro = novoBairro;
+    }
+
+    get cidade() {
+        return this.cidade;
+    }
+
+    set cidade(novaCidade) {
+        this.cidade = novaCidade;
+    }
+
+    get uf() {
+        return this.uf;
+    }
+
+    set uf(novaUf) {
+        this.uf = novaUf;
+    }
+
+    mostrarDadosEdificio() {
+        console.log(`Nome: ${this.nome}, Endereço: ${this.endereco}, Bairro: ${this.bairro}, Cidade: ${this.cidade}, UF: ${this.uf}`);
+    }
+}
