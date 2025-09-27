f553e74e04c5d1f3cbbcfe8d2fbe33ae726cbc9b
Victor Coelho <victor.fisica01@gmail.com>
2025-09-27 13:02:18 -0300

    modificando server.js

--- Trabalho Final/server/src/index.js
+++ Trabalho Final/server/src/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+const setupSwagger = require('./swagger'); // Using require
+
+// Route Imports
+const usersRouter = require('./routes/users');
+const contentsRouter = require('./routes/contents');
+const tagsRouter = require('./routes/tags');
+
 const app = express();
-const port = 3001;
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API do SGCPD está funcionando!');
-});
+// --- Setup Swagger ---
+setupSwagger(app);
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+// --- API Routes ---
+// This wires up all the Sequelize routes you created
+app.use('/api/users', usersRouter);
+app.use('/api/contents', contentsRouter);
+app.use('/api/tags', tagsRouter);
+
+// Set the port to 3001 to match your docker-compose file
+const PORT = process.env.PORT || 3001; 
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
